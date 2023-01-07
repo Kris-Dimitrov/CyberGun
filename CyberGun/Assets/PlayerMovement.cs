fc027c0469bcbf28ca720de61339fd2d2d568888
Kris-Dimitrov <74430991+Kris-Dimitrov@users.noreply.github.com>
2023-01-07 14:39:32 +0200

    Added Dash and Slam mechanics into Movement

--- CyberGun/Assets/PlayerMovement.cs
+++ CyberGun/Assets/PlayerMovement.cs
@@ -9,15 +9,29 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 6f;
     [SerializeField] float drag = 6f;
     [SerializeField] float speedMult = 10f;
+    [SerializeField] float dashMult = 10f;
     [SerializeField] float jumpForce = 10f;
+    [SerializeField] float gravity = 20f;
+    [SerializeField] float slamMultiplier = 3f;
+    [SerializeField] float airSpeedMultiplier = 0.5f;
+    [SerializeField] float maxDashCount = 10f;
+    [SerializeField] float dashConsumed = 10f;
+    [SerializeField] float dashRecoverySpeed = 0.1f;
+
 
     [Header("Keybinds")]
     [SerializeField] KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] KeyCode slamKey = KeyCode.LeftControl;
+    [SerializeField] KeyCode dashKey = KeyCode.LeftShift;
 
     bool isGrounded;
+    bool canDoubleJump;
+    bool canDash;
 
     float horizontalMovement;
     float verticalMovement;
+
+    float currentDashAmount;
 
     Vector3 moveDirection;
 
@@ -25,27 +39,29 @@ public class PlayerMovement : MonoBehaviour
 
     private void Start()
     {
+
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        rb.useGravity = false;
         ApplyDrag();
+
+        canDoubleJump = true;
+
+        currentDashAmount = maxDashCount;
     }
     void Update()
     {
         GetInput();
-
         isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight / 2 + 0.1f);
-
-        if (Input.GetKeyDown(jumpKey) && isGrounded)
-        {
-            Jump();
-        }
+        HandleJumps();
+        Slam();
+        Dash();
     }
-
-
 
     private void FixedUpdate()
     {
-        ApplyForces();
+        ApplyMovement();
+        DashCharger();
     }
 
     private void GetInput()
@@ -61,14 +77,73 @@ public class PlayerMovement : MonoBehaviour
         rb.drag = drag;
     }
 
-    private void ApplyForces() 
+    private void ApplyMovement()
     {
-        rb.AddForce(moveDirection.normalized * moveSpeed * speedMult, ForceMode.Acceleration);
+        if (isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * speedMult, ForceMode.Acceleration);
+        }
+        else
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * speedMult * airSpeedMultiplier, ForceMode.Acceleration);
+        }
+
     }
 
-    private void Jump() 
+    private void Jump()
     {
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
 
+    private void HandleJumps()
+    {
+        if (Input.GetKeyDown(jumpKey) && isGrounded)
+        {
+            Jump();
+        }
+
+        if (Input.GetKeyDown(jumpKey) && !isGrounded && canDoubleJump)
+        {
+            Jump();
+            canDoubleJump = false;
+        }
+
+        if (isGrounded)
+        {
+            canDoubleJump = true;
+        }
+    }
+
+    private void Slam()
+    {
+
+        if (Input.GetKey(slamKey) && !isGrounded)
+        {
+            Vector3 currentG = gravity * slamMultiplier * Vector3.up;
+            rb.AddForce(currentG, ForceMode.Acceleration);
+        }
+        else
+        {
+            Vector3 currentG = gravity * Vector3.up;
+            rb.AddForce(currentG, ForceMode.Acceleration);
+        }
+    }
+
+    private void Dash() 
+    {
+        if (currentDashAmount >= dashConsumed && Input.GetKeyDown(dashKey))
+        {
+            rb.AddForce(transform.forward * dashMult, ForceMode.Impulse);
+            currentDashAmount -= dashConsumed;
+        }
+
+    }
+
+    private void DashCharger() 
+    {
+        if (currentDashAmount < maxDashCount)
+        {
+            currentDashAmount += dashRecoverySpeed;
+        }
+    }
 }
